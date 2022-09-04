895c69a78c098ea1246b36b35202813ec4c551f5
coolpagani <coolpagani@gmail.com>
2022-09-04 16:41:09 +0300

    work_36

--- Program.cs
+++ Program.cs
@@ -1,5 +1,17 @@
-﻿int[] array = new int[new Random().Next(10, 11)]; int count = 0; int b = 0;
-for (int i = 0; i < array.Length; i++) array[i] = new Random().Next(100, 1000);
-for (int j = 0; j < array.Length; j++) count = array[j] % 2 == 0 ? b++ : 0;
-Console.Write(b);
-
+﻿int SumDigits(int[] array)
+{
+int c = 0; int result = 0;
+for (int j = 0; j < array.Length; j++)
+{
+    c = j % 2 != 0 ? array[j] : 0;
+    result +=c;
+}
+    return result;
+}
+int[] list = new int[new Random().Next(4, 11)];
+for (int i = 0; i < list.Length; i++)
+{
+    list[i] = new Random().Next(-100, 101);
+    Console.Write(list[i] + " ");
+}
+Console.WriteLine(SumDigits(list));
