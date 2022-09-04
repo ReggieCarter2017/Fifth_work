e6b8fd5603df3df7795c42b00b561ec034c51ad8
coolpagani <coolpagani@gmail.com>
2022-09-04 16:23:36 +0300

    work_34 commit

--- Program.cs
+++ Program.cs
@@ -1,1 +1,5 @@
-﻿
+﻿int[] array = new int[new Random().Next(10, 11)]; int count = 0; int b = 0;
+for (int i = 0; i < array.Length; i++) array[i] = new Random().Next(100, 1000);
+for (int j = 0; j < array.Length; j++) count = array[j] % 2 == 0 ? b++ : 0;
+Console.Write(b);
+
