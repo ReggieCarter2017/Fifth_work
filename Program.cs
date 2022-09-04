03fdad8b5e188016744bb42bac0528e7360a51b0
coolpagani <coolpagani@gmail.com>
2022-09-04 20:02:38 +0300

    work_38

--- Program.cs
+++ Program.cs
@@ -1,17 +1,26 @@
-﻿int SumDigits(int[] array)
+﻿int Minus(int[] array)
 {
-int c = 0; int result = 0;
-for (int j = 0; j < array.Length; j++)
-{
-    c = j % 2 != 0 ? array[j] : 0;
-    result +=c;
+    int temp = 0;
+    for (int j = 0; j < array.Length; j++)
+    {
+    for (int i = 0; i < array.Length-1; i++)
+    {
+        if (array[i] > array[i+1])
+        {
+        temp = array[i];
+        array[i] = array[i+1];
+        array[i+1] = temp;
+        }
+    }
+    }
+    return array[^1] - array[0];
 }
-    return result;
-}
-int[] list = new int[new Random().Next(4, 11)];
+
+int[] list = new int[new Random().Next(10, 11)];
 for (int i = 0; i < list.Length; i++)
 {
-    list[i] = new Random().Next(-100, 101);
-    Console.Write(list[i] + " ");
+    list[i] = new Random().Next(1, 100);
+    Console.WriteLine("Изначальное число: " + list[i]);
 }
-Console.WriteLine(SumDigits(list));
+
+Console.Write(Minus(list));
